42bb5f67cb79e2875100c50f666adb9423e8d3f0
Vsevolod Iakovlev <vsevolod.v.iakovlev@gmail.com>
2024-03-26 13:02:12 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 Toy microsimulation of labour mismatch.
 
 This module implements a simple microsimulation of labour mismatch based on a static version of the two-sided matching model 
-developed by Zinn et al. (2012). The purpose of the simulation is to produce matching outcomes of a labour market populated by 
+developed by Zinn (2012). The purpose of the simulation is to produce matching outcomes of a labour market populated by 
 a set of workers and a set of jobs with pre-defined characteristics and compute the output of various mismatch measures. 
 This can then be analysed to gain a deeper understanding of the construct of the mismatch measures.
 For more information, see https://github.com/vsevolodiakovlev/toysimlm/blob/main/README.md.
